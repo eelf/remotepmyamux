f87045507dd4e19bade65f9b0c2e7e30439ea090
Evgeniy Makhrov <e.makhrov@corp.badoo.com>
2019-06-29 00:08:49 +0300

    there's no joy ;)

--- main.go
+++ main.go
@@ -30,6 +30,81 @@ func pipe(stream, service net.Conn, label string) {
 	service.Close()
 }
 
+func server(controlNet, controlAddr, localNet, localAddr string) {
+	controlLn, err := net.Listen(controlNet, controlAddr)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("control connection listening", controlLn.Addr().String())
+	localLn, err := net.Listen(localNet, localAddr)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("exposed remote service at", localLn.Addr().String())
+
+	sessions := make(map[*yamux.Session]bool)
+	newSessionCh := make(chan *yamux.Session)
+	badSessionCh := make(chan *yamux.Session)
+	getSessionCh := make(chan chan *yamux.Session)
+	go func() {
+		for {
+			controlConn, err := controlLn.Accept()
+			if err != nil {
+				log.Println("control accept err:", err)
+				continue
+			}
+			log.Println("control connected from", controlConn.RemoteAddr().String())
+			session, err := yamux.Server(controlConn, nil)
+			if err != nil {
+				log.Println("mux server err:", err)
+				err = controlConn.Close()
+				if err != nil {
+					log.Println("control conn close err:", err)
+				}
+				continue
+			}
+			newSessionCh <- session
+		}
+	}()
+	go func() {
+		for {
+			clientConn, err := localLn.Accept()
+			if err != nil {
+				panic(err)
+			}
+			log.Println("client connected from", clientConn.RemoteAddr().String())
+			gotSessionCh := make(chan *yamux.Session)
+			var stream net.Conn
+			for {
+				getSessionCh <- gotSessionCh
+				session := <-gotSessionCh
+				stream, err = session.Open()
+				if err != nil {
+					badSessionCh <- session
+					continue
+				}
+				break
+			}
+			go pipe(stream, clientConn, "client")
+		}
+	}()
+	for {
+		select {
+		case session := <- newSessionCh:
+			sessions[session] = true
+		case gotSessionCh := <- getSessionCh:
+			for session, _ := range sessions {
+				gotSessionCh <- session
+				break
+			}
+		case session := <- badSessionCh:
+			if _, ok := sessions[session]; ok {
+				delete(sessions, session)
+			}
+		}
+	}
+}
+
 func main() {
 	var controlNet, controlAddr, localNet, localAddr, serviceNet, serviceAddr string
 	var tries, triesSleep uint
@@ -44,41 +119,7 @@ func main() {
 	flag.Parse()
 
 	if localAddr != "" {
-		controlLn, err := net.Listen(controlNet, controlAddr)
-		if err != nil {
-			panic(err)
-		}
-		log.Println("control connection listening", controlLn.Addr().String())
-		localLn, err := net.Listen(localNet, localAddr)
-		if err != nil {
-			panic(err)
-		}
-		log.Println("exposed remote service at", localLn.Addr().String())
-		for {
-			controlConn, err := controlLn.Accept()
-			if err != nil {
-				panic(err)
-			}
-			log.Println("control connected from", controlConn.RemoteAddr().String())
-			session, err := yamux.Server(controlConn, nil)
-			if err != nil {
-				panic(err)
-			}
-			go func(session *yamux.Session) {
-				for {
-					clientConn, err := localLn.Accept()
-					if err != nil {
-						panic(err)
-					}
-					log.Println("client connected from", clientConn.RemoteAddr().String())
-					stream, err := session.Open()
-					if err != nil {
-						panic(err)
-					}
-					go pipe(stream, clientConn, "client")
-				}
-			}(session)
-		}
+		server(controlNet, controlAddr, localNet, localAddr)
 	} else {
 		for tries >= 0 {
 			controlConn, err := net.Dial(controlNet, controlAddr)
